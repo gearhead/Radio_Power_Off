63f289358c4c515ac6c8e78211fccc4de26b4971
gearhead <ys3al35l@gmail.com>
2022-06-25 14:05:49 -0500

    v5 update
    Original v5 updates with a lot of variable name changes

--- Radio_PO.ino
+++ Radio_PO.ino
@@ -1,4 +1,4 @@
-/*
+ /*
  * Program to control relay to mimic GM "Retained Accessory Power"
  * http://www.the12volt.com/relays/relaydiagram30.html
  * using Digispark Pro http://digistump.com/wiki/digispark/tutorials/connecting
@@ -11,65 +11,83 @@
  * 1n4001 from +15 to radio lead
  * red +30, yellow +15, blue radio, black gnd 
  * Pin 5 acts as reset, so when this grounds, it powers off.
- * door to pin 3
+ * Moved door pin to pin 3
+ * 
+ * Made a schematic and added a lot of components to protest both the car and the tiny
+ * 
+ * pcint set for ignition off. When switched off, switch the pin to the door and await the 
+ * pin going low to turn off or the timer to expire. 
+ * 
+ * For some reason digispark will not register hi to low on pin 4
+ * 
+ * rev 4 worked but relied on reset and polling...
+ * rev 5 re-write to use pcint and to make it work -- had to move door to pin 3 from 4
  */
  
 // code to turn off radio when door is opened or after 15 min
-#define indPin 1  // on board led high to turn on
-#define relayPin 0
-#define runPin 2
-#define doorPin 4 // previously pin 5 a gnd on 5 will shut it down
+#define relayPin 0 // output to relay
+#define ledPin 1   // on board led high to turn on
+#define ignPin 2   // igniton on = HIGH
+#define doorPin 3  // gnd will shut it down
 
-bool engineOn = false;  // engine running flag
 unsigned long currentMillis;
-unsigned long nextMillis;
-int runDelay = 1000; // how often to run loop (led flash)
-unsigned long offTime;  // millis value when to turn off
+unsigned long previousMillis;
+unsigned long interval = 500;    // how often to flash led
 unsigned long offDelay = 900000; // 900000 is 15 min
-bool ledState = 0;
-bool ignState;
-bool lastignState;
-bool doorState = 1;
-bool lastdoorState;
+unsigned long offTime;           // calculated time 15 min after power off
+bool ledState = LOW;             // start blink off
+bool ignState = HIGH;            // assume ign ON as power up causes it to go on
+bool doorState = HIGH;           // waiting for a gnd
+volatile bool intSignal = HIGH;  // powered up and waiting for a turn off
 
-void setup() {
-            
+void setup() { 
   // initialize the digital pin as an output.
-  pinMode(doorPin,INPUT_PULLUP);  // high when running
-  pinMode(runPin, INPUT);  // high when running - this needs a pull down
-  pinMode(indPin, OUTPUT); //LED on DigiSpark board, LOW to turn on
-  pinMode(relayPin, OUTPUT); // LOW to turn on
+  pinMode(doorPin,INPUT);  // high when running
+  pinMode(ignPin, INPUT);  // high when running (15) - this needs a pull down
+  pinMode(relayPin, OUTPUT); // LOW to turn on relay
   digitalWrite(relayPin, LOW);  // turn on relay at power up
-  digitalWrite(indPin, HIGH);
-  currentMillis = millis();
-  offTime = currentMillis + offDelay;
-  nextMillis = currentMillis + runDelay;
+  pinMode(ledPin, OUTPUT); //LED on DigiSpark board, HIGH to turn on
+  digitalWrite(ledPin, HIGH); // turn on LED
+
+  cli();
+  PCMSK = 0b00000100;    // turn on PCINT interrupt on pin PB2 - wait for ign off 
+  GIMSK = 0b00100000;    // turns on pin change interrupts
+  sei();
 }
 
 void loop() {
-   currentMillis = millis();
-     if (currentMillis > nextMillis) { // do this once a second
-      doorState = digitalRead(doorPin);  // HIGH door closed, LOW door opened
-      delay(5);
-      if (doorState != lastdoorState) {
-      digitalWrite(indPin, !doorState);  // pin hi is led off
-      }
-      
-      ignState = digitalRead(runPin);
+  currentMillis = millis();
+  if (!intSignal && ignState) {  // we turned ign off
+    // start timer
+    offTime = currentMillis + offDelay;  // set turn off time
+    ignState = LOW;                      // ign flag off
+    interval = 250;                      // blink led faster after ign off
+    cli();
+    PCMSK = 0b00001000;    // trigger PCINT0_vect on pin PB3 - waiting for door 
+    sei();
+    intSignal = HIGH;      // set back to HIGH for door interrupt
+  }
 
-      if(ignState == 1) {
-        // ign still on increase delay shutoff time
-        offTime = currentMillis + offDelay;
-      }
-      
-     if (currentMillis >= offTime) {
-      // if door pin goes low or we exceed time turn off relay
-      //digitalWrite(indPin, LOW);  // turn off indicator
-      digitalWrite(relayPin, HIGH);  // open relay
-      }
-       nextMillis = currentMillis + runDelay;
-       lastdoorState = doorState;
-       lastignState = ignState;
-     }
+  if (!ignState && currentMillis >= offTime) { // ign off and timer expired
+    interval = 100;                             // blink even faster
+    digitalWrite(relayPin, HIGH);               // turn off relay - powers it all down
+  }
 
+  if (!ignState && !intSignal) {               // ign off waiting for a door 
+    digitalWrite(relayPin, HIGH);               // turn off relay - powers it all down
+  }
+
+  if (currentMillis - previousMillis >= interval) { // led blinking
+    // save the last time the LED blinked
+    previousMillis = currentMillis;
+    ledState = !ledState;
+    digitalWrite(ledPin, ledState);
+  }
+}
+
+/*
+ * Command: interrupt handler
+ */
+ISR(PCINT0_vect) {
+  intSignal = LOW;
 }
